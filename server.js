0991b4e6f79c7d5dd192a73569bd33bd47cc2b13
casture <micah_weatherhead@byu.edu>
2016-02-05 19:15:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('<strong>Name: </strong> Micah Weatherhead<br><strong>AMI ID:</strong> amzn-ami-hvm-2015.09.1.x86_64-gp2 (ami-60b6c60a)');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Example app listening on port 8080!');
 })
